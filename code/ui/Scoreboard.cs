3cc247f197e3015122a7f33275deff947d0c9528
bakscratch <lt_8@hotmail.co.uk>
2022-07-19 02:28:43 +0100

    Tweaked scoreboard ui

--- code/ui/Scoreboard.cs
+++ code/ui/Scoreboard.cs
@@ -5,10 +5,10 @@ public class Scoreboard : Sandbox.UI.Scoreboard<ScoreboardEntry>
 	protected override void AddHeader()
 	{
 		Header = Add.Panel( "header" );
-		Header.Add.Label( "player", "name" );
-		Header.Add.Label( "kills", "kills" );
-		Header.Add.Label( "deaths", "deaths" );
-		Header.Add.Label( "ping", "ping" );
+		Header.Add.Label( "Player", "name" );
+		Header.Add.Label( "Kills", "kills" );
+		Header.Add.Label( "Deaths", "deaths" );
+		Header.Add.Label( "Ping", "ping" );
 	}
 
 	RealTimeSince timeSinceSorted;
