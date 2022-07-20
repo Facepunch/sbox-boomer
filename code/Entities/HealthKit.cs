d07477a5964bef48dd84f360ab71047bf69a4a25
kurozael <kurozael@gmail.com>
2022-07-20 17:44:53 +0100

    Fixed HealthKit

--- code/Entities/HealthKit.cs
+++ code/Entities/HealthKit.cs
@@ -27,21 +27,30 @@ partial class HealthKit : AnimatedEntity, IRespawnableEntity
 	{
 		base.StartTouch( other );
 
-		if ( other is not BoomerPlayer pl ) return;
-		if ( pl.Health >= pl.MaxHealth ) return;
+		if ( IsServer )
+		{
+			if ( other is not BoomerPlayer pl ) return;
+			if ( pl.Health >= pl.MaxHealth ) return;
 
-		var newhealth = pl.Health + 25;
+			var newhealth = pl.Health + 25;
 
-		newhealth = newhealth.Clamp( 0, pl.MaxHealth );
-		
-		pl.Health = newhealth;
+			newhealth = newhealth.Clamp( 0, pl.MaxHealth );
 
-		PickEffect( pl );
+			pl.Health = newhealth;
 
+			PickEffect( pl );
+
+			PlayPickupSound();
+			PickupFeed.OnPickup( To.Single( pl ), $"+25 Health" );
+			ItemRespawn.Taken( this, RespawnTime );
+			Delete();
+		}
+	}
+
+	[ClientRpc]
+	private void PlayPickupSound()
+	{
 		Sound.FromWorld( "dm.item_health", Position );
-		PickupFeed.OnPickup( To.Single( pl ), $"+25 Health" );
-		ItemRespawn.Taken( this , RespawnTime);
-		Delete();
 	}
 
 	private void PickEffect( BoomerPlayer player )
