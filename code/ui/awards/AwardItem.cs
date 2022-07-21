e2bff469ca23bb8e103416ae024f2d627a2ba009
kurozael <kurozael@gmail.com>
2022-07-21 15:16:04 +0100

    Add basic award UI to be styled later

---
+++ code/ui/awards/AwardItem.cs
@@ -0,0 +1,36 @@
+﻿using Sandbox.UI;
+
+[UseTemplate]
+public class AwardItem : Panel
+{
+	public float EndTime { get; set; }
+
+	public Label Title { get; private set; }
+	public Label Text { get; private set; }
+	public Image Icon { get; private set; }
+	public Award Award { get; private set; }
+
+	public void Update( string title, string text )
+	{
+		Title.Text = title;
+		Text.Text = text;
+	}
+
+	public void SetAward( Award award )
+	{
+		Award = award;
+	}
+
+	public void SetIcon( Texture texture )
+	{
+		Icon.Texture = texture;
+	}
+
+	public override void Tick()
+	{
+		if ( !IsDeleting && Time.Now >= EndTime )
+		{
+			Delete();
+		}
+	}
+}
---
+++ code/ui/awards/AwardQueue.cs
@@ -0,0 +1,53 @@
+﻿using Sandbox.UI;
+
+public class AwardQueue : Panel
+{
+	public static AwardQueue Instance { get; private set; }
+
+	public Queue<AwardItem> Queue { get; private set; }
+
+	private Sound CurrentSound { get; set; }
+	private Panel Container { get; set; }
+
+	public AwardQueue()
+	{
+		Container = Add.Panel( "container" );
+		Instance = this;
+		Queue = new();
+	}
+
+	public void AddItem( AwardItem item )
+	{
+		if ( item.Award.ClearQueue )
+		{
+			Container.DeleteChildren( true );
+			Queue.Clear();
+		}
+
+		Queue.Enqueue( item );
+	}
+
+	public void Next()
+	{
+		if ( Queue.Count > 0 )
+		{
+			var item = Queue.Dequeue();
+			item.EndTime = Time.Now + 4f;
+
+			CurrentSound.Stop();
+			CurrentSound = Sound.FromScreen( item.Award.SoundName );
+
+			Container.AddChild( item );
+		}
+	}
+
+	public override void Tick()
+	{
+		if ( Queue.Count > 0 && Container.ChildrenCount == 0 )
+		{
+			Next();
+		}
+
+		base.Tick();
+	}
+}
