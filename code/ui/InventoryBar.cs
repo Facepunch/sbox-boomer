be9e9ca79df840f44a29a7d7db6988f6534ddfd4
kurozael <kurozael@gmail.com>
2022-07-22 14:59:44 +0100

    Clean up the inventory system for Boomer and implement Q to switch

--- code/ui/InventoryBar.cs
+++ code/ui/InventoryBar.cs
@@ -1,23 +1,16 @@
 ﻿using Sandbox.UI;
 
-/// <summary>
-/// The main inventory panel, top left of the screen.
-/// </summary>
 public class InventoryBar : Panel
 {
-	List<InventoryColumn> columns = new();
-	List<DeathmatchWeapon> Weapons = new();
+	public bool IsOpen { get; private set; }
 
-	public bool IsOpen;
-	DeathmatchWeapon SelectedWeapon;
+	private DeathmatchWeapon SelectedWeapon { get; set; }
+	private List<DeathmatchWeapon> Weapons { get; set; } = new();
+	private Panel Container { get; set; }
 
 	public InventoryBar()
 	{
-		for ( int i = 0; i < 6; i++ )
-		{
-			var icon = new InventoryColumn( i, this );
-			columns.Add( icon );
-		}
+		Container = Add.Panel( "container" );
 	}
 
 	public override void Tick()
@@ -32,16 +25,18 @@ public class InventoryBar : Panel
 		Weapons.Clear();
 		Weapons.AddRange( player.Children.Select( x => x as DeathmatchWeapon ).Where( x => x.IsValid() && x.IsUsable() ) );
 
-		foreach ( var weapon in Weapons )
+		if ( Weapons.Count != Container.ChildrenCount )
 		{
-			columns[weapon.Bucket].UpdateWeapon( weapon );
+			Container.DeleteChildren( true );
+
+			foreach ( var weapon in Weapons.OrderBy( x => x.Order ) )
+			{
+				var icon = new InventoryIcon( weapon );
+				Container.AddChild( icon );
+			}
 		}
 	}
 
-	/// <summary>
-	/// IClientInput implementation, calls during the client input build.
-	/// You can both read and write to input, to affect what happens down the line.
-	/// </summary>
 	[Event.BuildInput]
 	public void ProcessClientInput( InputBuilder input )
 	{
@@ -50,9 +45,8 @@ public class InventoryBar : Panel
 		bool wantOpen = IsOpen;
 		var localPlayer = Local.Pawn as Player;
 
-		// If we're not open, maybe this input has something that will 
-		// make us want to start being open?
 		wantOpen = wantOpen || input.MouseWheel != 0;
+		wantOpen = wantOpen || input.Pressed( InputButton.Menu );
 		wantOpen = wantOpen || input.Pressed( InputButton.Slot1 );
 		wantOpen = wantOpen || input.Pressed( InputButton.Slot2 );
 		wantOpen = wantOpen || input.Pressed( InputButton.Slot3 );
@@ -66,46 +60,36 @@ public class InventoryBar : Panel
 			return;
 		}
 
-		// We're not open, but we want to be
 		if ( IsOpen != wantOpen )
 		{
 			SelectedWeapon = localPlayer?.ActiveChild as DeathmatchWeapon;
 			IsOpen = true;
 		}
 
-		// Not open fuck it off
 		if ( !IsOpen ) return;
 
-		//
-		// Fire pressed when we're open - select the weapon and close.
-		//
-//if ( input.Down( InputButton.PrimaryAttack ) )
-	//	{
-			//input.SuppressButton( InputButton.PrimaryAttack );
-			input.ActiveChild = SelectedWeapon;
-			//IsOpen = false;
-			//Sound.FromScreen( "dm.ui_select" );
-		//	return;
-	//	}
-
 		var sortedWeapons = Weapons.OrderBy( x => x.Order ).ToList();
-
-		// get our current index
 		var oldSelected = SelectedWeapon;
-		int SelectedIndex = sortedWeapons.IndexOf( SelectedWeapon );
-		SelectedIndex = SlotPressInput( input, SelectedIndex, sortedWeapons );
+		var selectedIndex = sortedWeapons.IndexOf( SelectedWeapon );
 
-		// forward if mouse wheel was pressed
-		SelectedIndex -= input.MouseWheel;
-		SelectedIndex = SelectedIndex.UnsignedMod( Weapons.Count );
+		selectedIndex = SlotPressInput( input, selectedIndex, sortedWeapons );
+		selectedIndex -= input.MouseWheel;
 
-		SelectedWeapon = sortedWeapons[SelectedIndex];
+		if ( input.Pressed( InputButton.Menu ) )
+			selectedIndex++;
 
-		for ( int i = 0; i < 6; i++ )
+		selectedIndex = selectedIndex.UnsignedMod( Weapons.Count );
+
+		SelectedWeapon = sortedWeapons[selectedIndex];
+
+		var icons = Container.ChildrenOfType<InventoryIcon>();
+
+		foreach ( var icon in icons )
 		{
-			columns[i].TickSelection( SelectedWeapon );
+			icon.TickSelection( SelectedWeapon );
 		}
 
+		input.ActiveChild = SelectedWeapon;
 		input.MouseWheel = 0;
 
 		if ( oldSelected != SelectedWeapon )
@@ -114,48 +98,15 @@ public class InventoryBar : Panel
 		}
 	}
 
-	int SlotPressInput( InputBuilder input, int SelectedIndex, List<DeathmatchWeapon> sortedWeapons )
+	private int SlotPressInput( InputBuilder input, int index, List<DeathmatchWeapon> sortedWeapons )
 	{
-		var columninput = -1;
+		if ( input.Pressed( InputButton.Slot1 ) ) index = 0;
+		if ( input.Pressed( InputButton.Slot2 ) ) index = 1;
+		if ( input.Pressed( InputButton.Slot3 ) ) index = 2;
+		if ( input.Pressed( InputButton.Slot4 ) ) index = 3;
+		if ( input.Pressed( InputButton.Slot5 ) ) index = 4;
+		if ( input.Pressed( InputButton.Slot6 ) ) index = 5;
 
-		if ( input.Pressed( InputButton.Slot1 ) ) columninput = 0;
-		if ( input.Pressed( InputButton.Slot2 ) ) columninput = 1;
-		if ( input.Pressed( InputButton.Slot3 ) ) columninput = 2;
-		if ( input.Pressed( InputButton.Slot4 ) ) columninput = 3;
-		if ( input.Pressed( InputButton.Slot5 ) ) columninput = 4;
-		if ( input.Pressed( InputButton.Slot6 ) ) columninput = 5;
-
-		if ( columninput == -1 ) return SelectedIndex;
-
-		if ( SelectedWeapon.IsValid() && SelectedWeapon.Bucket == columninput )
-		{
-			return NextInBucket( sortedWeapons );
-		}
-
-		// Are we already selecting a weapon with this column?
-		var firstOfColumn = sortedWeapons.Where( x => x.Bucket == columninput ).FirstOrDefault();
-		if ( firstOfColumn == null )
-		{
-			// DOOP sound
-			return SelectedIndex;
-		}
-
-		return sortedWeapons.IndexOf( firstOfColumn );
-	}
-
-	int NextInBucket( List<DeathmatchWeapon> sortedWeapons )
-	{
-		Assert.NotNull( SelectedWeapon );
-
-		DeathmatchWeapon first = null;
-		DeathmatchWeapon prev = null;
-		foreach ( var weapon in sortedWeapons.Where( x => x.Bucket == SelectedWeapon.Bucket ) )
-		{
-			if ( first == null ) first = weapon;
-			if ( prev == SelectedWeapon ) return sortedWeapons.IndexOf( weapon );
-			prev = weapon;
-		}
-
-		return sortedWeapons.IndexOf( first );
+		return index;
 	}
 }
