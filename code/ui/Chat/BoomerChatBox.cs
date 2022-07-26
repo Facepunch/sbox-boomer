ef7d17809e1df589f97e5fb18b5256bd770a1272
bakscratch <lt_8@hotmail.co.uk>
2022-07-27 00:05:37 +0100

    Fixed game not uploading

--- code/ui/Chat/BoomerChatBox.cs
+++ code/ui/Chat/BoomerChatBox.cs
@@ -103,17 +103,3 @@ namespace Boomer.UI
 	}
 }
 
-namespace Sandbox.Hooks
-{
-	public static partial class Chat
-	{
-		public static event Action OnOpenChat;
-
-		[ConCmd.Client( "openchat" )]
-		internal static void MessageMode()
-		{
-			OnOpenChat?.Invoke();
-		}
-
-	}
-}
