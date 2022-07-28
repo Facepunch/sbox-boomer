9db63617bfd0e470657cd12a0d716cb212283ee4
bakscratch <lt_8@hotmail.co.uk>
2022-07-28 14:55:23 +0100

    Reduced max buckshot ammo and increased grenades

--- code/Player/BoomerPlayer.Ammo.cs
+++ code/Player/BoomerPlayer.Ammo.cs
@@ -103,10 +103,10 @@
 		switch ( ammo )
 		{
 			case AmmoType.Pistol: return 250;
-			case AmmoType.Buckshot: return 100;
+			case AmmoType.Buckshot: return 25;
 			case AmmoType.Nails: return 200;
 			case AmmoType.Rockets: return 20;
-			case AmmoType.Grenade: return 10;
+			case AmmoType.Grenade: return 15;
 			case AmmoType.Rails: return 15;
 			case AmmoType.Lightning: return 300;
 
