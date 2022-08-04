79033fd2c820b33b421dbfed0b4b98770438e4e2
Jake Evans <jacob.ee92@gmail.com>
2022-08-04 11:51:01 -0600

    Some setting defaults

--- code/GameSettings.cs
+++ code/GameSettings.cs
@@ -5,16 +5,16 @@ internal class ClientSettings
 {
 
 	public WeaponPositionSetting WeaponPosition { get; set; }
-	public bool TennisBallMode { get; set; }
+	public bool TennisBallMode { get; set; } 
 	// todo: TennisBallModeColor color editor
-	public bool ShowHitMarkers { get; set; }
-	public bool ShowDamageNumbers { get; set; }
+	public bool ShowHitMarkers { get; set; } = true;
+	public bool ShowDamageNumbers { get; set; } = true;
 	public bool BatchDamageNumbers { get; set; }
 	// todo: CrosshairColor color editor
-	public bool Speedometer { get; set; }
+	public bool Speedometer { get; set; } = false;
 	public GoreModeSetting GoreMode { get; set; }
-	public bool WalkBob { get; set; }
-	public bool HearOwnFootsteps { get; set; }
+	public bool WalkBob { get; set; } = true;
+	public bool HearOwnFootsteps { get; set; } = true;
 
 	public void Save() => Cookie.Set( "boomer.clientsettings", this );
 
