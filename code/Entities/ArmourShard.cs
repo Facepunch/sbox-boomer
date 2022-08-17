101832805601245bdac89f26b0b251bb67e9adf5
bakscratch <lt_8@hotmail.co.uk>
2022-08-17 15:52:51 +0100

    Armour Shard +5 armour

---
+++ code/Entities/ArmourShard.cs
@@ -0,0 +1,77 @@
+﻿namespace Boomer;
+
+/// <summary>
+/// Gives 25 Armour
+/// </summary>
+[Library( "boomer_armourshard" ), HammerEntity]
+[EditorModel( "models/gameplay/armour_shard/dm_armour_shard.vmdl" )]
+[Title( "Armour Shard" ), Category( "PickUps" )]
+partial class ArmourShard : AnimatedEntity, IRespawnableEntity
+{
+	public static readonly Model WorldModel = Model.Load( "models/gameplay/armour_shard/armour_shard.vmdl" );
+
+	[Property]
+	public int RespawnTime { get; set; } = 30;
+	public override void Spawn()
+	{
+		base.Spawn();
+
+		Model = WorldModel;
+
+		PhysicsEnabled = true;
+		UsePhysicsCollision = true;
+
+		Tags.Add( "trigger" );
+
+		if ( DeathmatchGame.InstaGib )
+		{
+			Delete();
+		}
+	}
+
+	public override void StartTouch( Entity other )
+	{
+		base.StartTouch( other );
+
+		if ( IsServer )
+		{
+			if ( other is not BoomerPlayer player ) return;
+			if ( player.Armour >= 100 ) return;
+
+			var newhealth = player.Armour + 25;
+
+			newhealth = newhealth.Clamp( 0, 100 );
+
+			player.Armour = newhealth;
+
+			PlayPickupSound();
+
+			PickupFeed.OnPickup( To.Single( player ), $"+5 Armour" );
+			ItemRespawn.Taken( this, RespawnTime );
+
+			OnPickUpRpc( To.Single( other ) );
+
+			Delete();
+		}
+	}
+
+	[ClientRpc]
+	public void OnPickUpRpc()
+	{
+		Host.AssertClient();
+		_ = ChangedArmourAnim();
+	}
+
+	protected static async Task ChangedArmourAnim()
+	{
+		ArmourHud.Current.Value.SetClass( "gained", true );
+		await GameTask.DelaySeconds( 0.25f );
+		ArmourHud.Current.Value.SetClass( "gained", false );
+	}
+
+	[ClientRpc]
+	private void PlayPickupSound()
+	{
+		Sound.FromWorld( "armour.pickup", Position );
+	}
+}
