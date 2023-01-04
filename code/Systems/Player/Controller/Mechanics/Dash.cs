5cee4894fd27877c0ccaa3938381645e9b70301f
Tony Ferguson <devultj@gmail.com>
2023-01-04 19:48:44 +0000

    Restored DashCount, dash effects

--- code/Systems/Player/Controller/Mechanics/Dash.cs
+++ code/Systems/Player/Controller/Mechanics/Dash.cs
@@ -5,17 +5,51 @@ namespace Facepunch.Boomer.Mechanics;
 
 public partial class DashMechanic : PlayerControllerMechanic
 {
+	[Net] public int DashCount { get; set; }
+
+	public int MaxDashes => 2;
+	public float DashRechargeCooldown => 2;
+
 	protected override bool ShouldStart()
 	{
+		if ( DashCount < 1 ) return false;
 		if ( !Input.Pressed( InputButton.Run ) ) return false;
 
 		return true;
+	}
+
+	protected float GetPower()
+	{
+		return Controller.GroundEntity.IsValid() ? 1500 : 750;
 	}
 
 	protected override void OnStart()
 	{
 		// Give a speed boost
 		var wish = Controller.GetWishInput();
-		Controller.Velocity += wish * 1250.0f;
+		Controller.Velocity += wish * GetPower();
+
+		DashCount--;
+		DashEffect();
+	}
+
+	[Event.Tick.Server]
+	public void TickServer()
+	{
+		if ( TimeSinceStop > DashRechargeCooldown && DashCount != MaxDashes )
+		{
+			DashCount = 2;
+
+			if ( Controller.Player.IsLocalPawn )
+				Sound.FromScreen( "dashrecharge" ).SetVolume( 1f );
+		}
+	}
+
+	private void DashEffect()
+	{
+		if ( Game.IsServer || !Controller.Player.IsLocalPawn ) return;
+
+		Particles.Create( "particles/gameplay/screeneffects/dash/ss_dash.vpcf", Controller.Player );
+		Sound.FromWorld( "jump.double", Controller.Position );
 	}
 }
