ccd244ef6b2f7fed4e25250d557f0a31b1501aa2
bakscratch <lt_8@hotmail.co.uk>
2023-01-06 09:02:17 +0000

    Added missing ammo pickups

--- code/Systems/Pickups/AmmoPickup.cs
+++ code/Systems/Pickups/AmmoPickup.cs
@@ -64,3 +64,36 @@ partial class AmmoRockets : AmmoPickup
 	public override int AmmoAmount => 5;
 	public override Model WorldModel => Model.Load( "models/gameplay/ammo/rockets/bm_rockets.vmdl" );
 }
+
+[Library( "boomer_rails" ), HammerEntity]
+[EditorModel( "models/gameplay/ammo/rails/bm_rails.vmdl" )]
+[Title( "Rails" ), Category( "Ammo" )]
+partial class AmmoRails : AmmoPickup
+{
+	public override string ResourceIdent => "sniper";
+	public override string AmmoName => "#Ammo.Rails";
+	public override int AmmoAmount => 5;
+	public override Model WorldModel => Model.Load( "models/gameplay/ammo/rails/bm_rails.vmdl" );
+}
+
+[Library( "boomer_lightning" ), HammerEntity]
+[EditorModel( "models/gameplay/ammo/lightning/bm_lightning.vmdl" )]
+[Title( "Lightning" ), Category( "Ammo" )]
+partial class AmmoLightning : AmmoPickup
+{
+	public override string ResourceIdent => "lightning";
+	public override string AmmoName => "#Ammo.Lightning";
+	public override int AmmoAmount => 100;
+	public override Model WorldModel => Model.Load( "models/gameplay/ammo/lightning/bm_lightning.vmdl" );
+}
+
+[Library( "boomer_ammobuckshot" ), HammerEntity]
+[EditorModel( "models/gameplay/ammo/buckshot/bm_buckshot.vmdl" )]
+[Title( "Buckshot" ), Category( "Ammo" )]
+partial class AmmoBuckshot : AmmoPickup
+{
+	public override string ResourceIdent => "shotgun";
+	public override string AmmoName => "#Ammo.Buckshot";
+	public override int AmmoAmount => 5;
+	public override Model WorldModel => Model.Load( "models/gameplay/ammo/buckshot/bm_buckshot.vmdl" );
+}
