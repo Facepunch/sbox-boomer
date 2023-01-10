6767608149b188bbaefa3c14887745d149c12f9e
Tony Ferguson <devultj@gmail.com>
2023-01-10 15:27:11 +0000

    Give the player the old Boomer outfit

--- code/Systems/Player/Player.Clothing.cs
+++ code/Systems/Player/Player.Clothing.cs
@@ -15,6 +15,57 @@ public partial class Player
 
 		Clothing.ClearEntities();
 		Clothing.LoadFromClient( Client );
-		Clothing.DressEntity( this );
+		
+		PlayerColor = Color.White;
+		SetupClothing();
+	}
+
+	[Net] public Color PlayerColor { get; set; }
+
+	private static Material SkinMat = Material.Load( "models/gameplay/citizen/textures/citizen_skin.vmat" );
+	private static Material EyeMat = Material.Load( "models/gameplay/citizen/textures/eyes/citizen_eyes_advanced.vmat" );
+
+	public void SetupClothing()
+	{
+		SetBodyGroup( "Hands", 1 );
+		SetBodyGroup( "Feet", 1 );
+
+		SetMaterialOverride( SkinMat, "skin" );
+		SetMaterialOverride( EyeMat, "eyes" );
+
+		var pants = new ModelEntity();
+		pants.SetModel( "models/cosmetics/outfit/boomeroutfit_pants.vmdl" );
+		pants.SetParent( this, true );
+		pants.EnableHideInFirstPerson = true;
+		//pants.SceneObject.Attributes.Set( "RimColor1", PlayerColor );
+		pants.EnableShadowInFirstPerson = true;
+
+		var shoes = new ModelEntity();
+		shoes.SetModel( "models/cosmetics/outfit/boomeroutfit_shoes.vmdl" );
+		shoes.SetParent( this, true );
+		shoes.EnableHideInFirstPerson = true;
+		//shoes.SceneObject.Attributes.Set( "RimColor1", PlayerColor );
+		shoes.EnableShadowInFirstPerson = true;
+
+		var helmet = new ModelEntity();
+		helmet.SetModel( "models/cosmetics/outfit/boomeroutfit_helmet.vmdl" );
+		helmet.SetParent( this, true );
+		helmet.EnableHideInFirstPerson = true;
+		//helmet.SceneObject.Attributes.Set( "RimColor1", PlayerColor );
+		helmet.EnableShadowInFirstPerson = true;
+
+		var gloves = new ModelEntity();
+		gloves.SetModel( "models/cosmetics/outfit/boomeroutfit_gloves.vmdl" );
+		gloves.SetParent( this, true );
+		gloves.EnableHideInFirstPerson = true;
+		//gloves.SceneObject.Attributes.Set( "RimColor1", PlayerColor );
+		gloves.EnableShadowInFirstPerson = true;
+
+		var chest = new ModelEntity();
+		chest.SetModel( "models/cosmetics/outfit/boomeroutfit_chest.vmdl" );
+		chest.SetParent( this, true );
+		chest.EnableHideInFirstPerson = true;
+		//chest.SceneObject.Attributes.Set( "RimColor1", PlayerColor );
+		chest.EnableShadowInFirstPerson = true;
 	}
 }
