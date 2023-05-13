d6664899efe620cafeecce39dbf1eb72437c26ca
Tony Ferguson <devultj@gmail.com>
2023-05-13 23:10:19 +0100

    New scoreboard style

--- code/Systems/TeamSystem/Team.cs
+++ code/Systems/TeamSystem/Team.cs
@@ -20,7 +20,7 @@ public static class TeamExtensions
 	{
 		return team switch
 		{
-			Team.Cyan => new Color32( 20, 240, 240 ),
+			Team.Cyan => new Color32( 55, 125, 255 ),
 			Team.Yellow => new Color32( 240, 240, 20 ),
 			Team.Green => new Color32( 106, 241, 144 ),
 			Team.Purple => new Color32( 147, 95, 167 ),
@@ -32,10 +32,10 @@ public static class TeamExtensions
 	{
 		return team switch
 		{
-			Team.Cyan => "Team Cyan",
-			Team.Yellow => "Team Yellow",
-			Team.Green => "Team Green",
-			Team.Purple => "Team Purple",
+			Team.Cyan => "Blue Team",
+			Team.Yellow => "Yellow Team",
+			Team.Green => "Green Team",
+			Team.Purple => "Purple Team",
 			_ => null
 		};
 	}
